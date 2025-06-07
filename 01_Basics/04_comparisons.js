ef6a5dba1b2265c83c7626ad9b828d1d1e197351
Raghav Sharan Mishra <raghavsharan2016@gmail.com>
2025-06-07 16:39:34 +0530

    Comparisons

---
+++ 01_Basics/04_comparisons.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// console.log(undefined > 0)
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+
+// === -> this is strict checking, or comparison 
+//how?
+console.log("2" === 2)  //this is strict comparison 
+console.log("2" == 2)
