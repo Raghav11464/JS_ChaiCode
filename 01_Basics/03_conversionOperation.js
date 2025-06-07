c110489f9ecdecfc4dcf5e62c72cb963cdb3318d
Raghav Sharan Mishra <raghavsharan2016@gmail.com>
2025-06-07 15:45:22 +0530

    Conversion Operation

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,1 +1,28 @@
-let score = 33 
+let score = null
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)  //this returns NaN, one of the limitation of JS
+
+//NaN => means Not a Number 
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn = "raghav"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//1 => true, 0 => false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)//Number has been converted to String 
+
+
