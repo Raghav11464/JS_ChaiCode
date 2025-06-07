c69d97b5b3fc60f349f8ee52f152b97a721225b6
Raghav Sharan Mishra <raghavsharan2016@gmail.com>
2025-06-07 16:26:23 +0530

    Operations

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"    //treat all JS code as newer version 
+
+// alert(3 + 3) //we are using nodejs, not browser
+
+console.log(3 + 3)
+console.log("Raghav")
+
+let name = "Raghav" //string => ""
+let age = 20    //number, we have bigInt also 
+let price = 4.99
+let isLoggedIn = true   //boolean 
+let state;
+//object
+//symbol => unique 
+
+let str = "Raghav Sharan Mishra"
+console.log("length of string is: "+str.length)
+
+console.log("Raghav is "+age+" years old")  //string concatenation 
+
+//null => this is stand-alone value in JS 
+
+console.log(typeof "Raghav")
+console.log(typeof null)    //this returns object, which is one of the fault in JS 
+console.log(typeof 4.99)  //number
+console.log(typeof 1000)  //number 
+
--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -22,7 +22,31 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)//Number has been converted to String 
+// console.log(stringNumber)
+// console.log(typeof stringNumber)//Number has been converted to String 
 
+// ************************* Operations *************************
+
+let value = 3
+let negVal = -value
+// console.log(negVal)  //pos converted to neg value 
+
+let str1 = "hello"
+let str2 = ", Raghav"
+let str3 = str1+str2
+//console.log(str3)   //string concatenation 
+
+console.log("1"+2)
+console.log(1 + "2")
+console.log("1" + "2")  //normal concatenation 
+//number added to string
+//string added to number 
+
+console.log(1 + 2 + "2")    //Number + String, that is why 1+2=3and then 3+"2" ==> 32
+console.log("1" + 2 + 2)    //String + Number ==> 122
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
+//prefix and postfix
 
