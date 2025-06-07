b6260a1bca215f4693a7b3a6e6f52d853ce8720e
Raghav Sharan Mishra <raghavsharan2016@gmail.com>
2025-06-07 16:54:52 +0530

    Datatypes Summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,40 @@
+//Primitive Datatypes
+
+//7 Types:- Number, String, Boolean, null, undefined, Symbol, BigInt
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;  //this is undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//
+
+const bigNumber = 343423424324233423n
+
+
+//Reference (Non Primitive)
+
+//Array, Objects, Functions
+
+const names = ["Raghav", "Sharan", "Mishra"];    //Array 
+
+let myObj = {
+    name: "Raghav",
+    age: 22,
+    city: "Bengaluru"
+}   //this is an Object
+
+const myFunction = function(){
+    console.log("Hello World");
+}   //this is a Function 
+
+console.log(typeof bigNumber);
+console.log(typeof anotherId);
+console.log(typeof null);   //object
+console.log(typeof undefined);  //undefined
+console.log(typeof myObj);  //object
+console.log(typeof myFunction); //function
+console.log(typeof names);  //object
+
