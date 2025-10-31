01352e2954cb0756badb6d2019dda3f94eedb4be
Raghav Sharan Mishra <raghavsharan2016@gmail.com>
2025-10-31 15:54:47 +0530

    Arrays Basics in JS

--- 02_Basics/01_arrays.js
+++ 02_Basics/01_arrays.js
@@ -1,6 +1,18 @@
+/* Shallow copy:
+-->copy whose properties share the same 
+references as those of the source object from which the copy was made. 
+As a result, when you change either the source or the copy, you may also 
+cause the other object to change too
+
+Deep copy:
+-->copy whose properties do not share the 
+same references as those of the source object from which the copy was 
+made. As a result, when you change either the source or the copy, you 
+can be assured you're not causing the other object to change too.
+*/
 //Arrays in JS
 //JavaScript arrays are resizable and can contain a mix of different data types
-//means they are heterogeneous in nature, they can contain unlike elements in the array 
+//means they are heterogeneous in nature, they can store unlike elements in the array 
 
 const myArr = [0, 1, 2, 3, 4, 5];
 console.log(myArr);
@@ -18,7 +30,7 @@ console.log(myArr2[3]);
 
 myArr.push(6);    //appends new elements to the end of the array 
 myArr.push(7);   
-myArr.pop();
+myArr.pop();    //removes the last element from the array 
 console.log(myArr);
 
 myArr.unshift("Starting"); //add new elements to the starting of the array 
@@ -41,4 +53,29 @@ console.log(myArr);
 console.log(newArr);    //this is a string 
 
 //slice and splice methods 
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+const newAnimals = animals.slice(2, 4);
+console.log(newAnimals);
 
+
+const myFish = ["angel", "clown", "drum", "sturgeon"];
+const removed = myFish.splice(2, 1, "trumpet");
+//remove 1 element at index 2, and insert "trumpet"
+// console.log(removed);
+console.log(myFish);
+
+
+
+/*
+Summary 
+1. push()-push element at the end of the array
+2. pop()-delete element from end of array
+3. unshift()-adds element at the start of array
+4. shift()-delete element from start of the array
+5. join() : converts array into string
+6. slice(): returns a new array containing the extracted 
+elements.The original array will not be modified.
+7. splice(): changes the contents of an array by removin
+g or replacing existing elements and/or adding new elem
+ents in place. Here original array is changed.
+*/
